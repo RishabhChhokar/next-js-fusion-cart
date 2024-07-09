c342b1c1844b9705b3a801c89ff45ecde7d6d8e4
Rishabh Chhokar <rishabhchokkar@gmail.com>
2024-07-09 16:52:40 +0530

    Added ProductItem component to display products

--- app/(front)/page.jsx
+++ app/(front)/page.jsx
@@ -1,7 +1,14 @@
+import ProductItem from "@/components/products/ProductItem";
+import data from "@/lib/data";
 export default function Home() {
   return (
     <>
-      <h2 className="text-2xl py-2">Tried really hard</h2>
+      <h2 className="text-2xl py-2">Latest Products</h2>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map(product => {
+          return <ProductItem key = {product.slug} product = {product}/>
+        })}
+      </div>
     </>
   );
 }
